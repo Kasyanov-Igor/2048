cc9db61abe79059e21ae88616590f004602cfa65
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2024-10-06 20:36:49 +0300

    Added project files. Closed #1

---
+++ 2048/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace _2048
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
