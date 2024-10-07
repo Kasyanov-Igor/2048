8262caf2b97fd1e2544a93d12c1b40755ed2c1de
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2024-10-07 21:22:25 +0300

    Added the walking method. Closed #4

--- 2048/LogicGameField.cs
+++ 2048/LogicGameField.cs
@@ -1,65 +1,252 @@
 ﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace _2048
 {
-    public class LogicGameField
-    {
-        private readonly byte[,] _field;
+	public class LogicGameField
+	{
+		private readonly uint[,] _field;
 
-        public const byte COUNT_ROWS = 4;
+		public const byte COUNT_ROWS = 4;
 
-        public const byte COUNT_COLLUMNS = 4;
+		public const byte COUNT_COLLUMNS = 4;
 
-        public LogicGameField()
-        {
-            this._field = new byte[COUNT_ROWS, COUNT_COLLUMNS];
+		private uint _gameСount = 0;
 
-            for (int i = 0; i < COUNT_ROWS; i++)
-            {
-                for (int k = 0; k < COUNT_COLLUMNS; k++)
-                {
-                    // Set empty cells
-                    this._field[i, k] = 0;
-                }
-            }
-        }
+		Random random = new Random();
 
-        public bool AddField()
-        {
-            Random random = new Random();
-            byte randRow = Convert.ToByte(random.Next(COUNT_ROWS));
-            byte randCollumn = Convert.ToByte(random.Next(COUNT_COLLUMNS));
+		public LogicGameField()
+		{
+			this._field = new uint[COUNT_ROWS, COUNT_COLLUMNS];
 
-            while (CheckEmptySells() == true)
-            {
-                if (this._field[randRow, randCollumn] == 0)
-                {
-                    this._field[randRow, randCollumn] = 1;
+			for (int i = 0; i < COUNT_ROWS; i++)
+			{
+				for (int k = 0; k < COUNT_COLLUMNS; k++)
+				{
+					// Set empty cells
+					this._field[i, k] = 0;
+				}
+			}
+		}
 
-                    return true;
-                }
-            }
-    
-            return false;
-        }
+		public bool AddField()
+		{
+			byte randRow = Convert.ToByte(random.Next(COUNT_ROWS));
+			byte randCollumn = Convert.ToByte(random.Next(COUNT_COLLUMNS));
 
-        public bool CheckEmptySells()
-        {
-            foreach (byte cell in this._field)
-            {
-                if (cell == 0)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
+			if (this._field[randRow, randCollumn] == 0)
+			{
+				if (random.Next(100) > 10)
+				{
+					this._field[randRow, randCollumn] = 2;
+				}
+				else
+				{
+					this._field[randRow, randCollumn] = 4;
+				}
+				return true;
+			}
+			else
+			{
+				for (int i = randRow; i < COUNT_ROWS; i++)
+				{
+					for (int j = randCollumn; j < COUNT_COLLUMNS; j++)
+					{
+						if (this._field[i, j] == 0)
+						{
+							if (random.Next(100) > 10)
+							{
+								this._field[i, j] = 2;
+							}
+							else
+							{
+								this._field[i, j] = 4;
+							}
+							return true;
+						}
+					}
+				}
+				for (int i = 0; i < COUNT_ROWS; i++)
+				{
+					for (int j = 0; j < COUNT_COLLUMNS; j++)
+					{
+						if (this._field[i, j] == 0)
+						{
+							if (random.Next(100) > 10)
+							{
+								this._field[i, j] = 2;
+							}
+							else
+							{
+								this._field[i, j] = 4;
+							}
+							return true;
+						}
+					}
+				}
 
-        public byte[,] GetField()
-        {
-            return this._field;
+			}
+			return false;
+		}
 
-        }
+		public void PushUp()
+		{
+			for (int i = 1; i < COUNT_ROWS; i++)
+			{
+				for (int j = 0; j < COUNT_COLLUMNS; j++)
+				{
+					if (this._field[i, j] != 0)
+					{
+						int doubleI = i;
 
-    }
+						while (doubleI > 0)
+						{
+							if (this._field[doubleI, j] != this._field[doubleI - 1, j] && this._field[doubleI - 1, j] != 0)
+							{
+								break;
+							}
+
+							if (this._field[doubleI, j] == this._field[doubleI - 1, j])
+							{
+								this._field[doubleI - 1, j] *= 2;
+								this._field[doubleI, j] = 0;
+								this._gameСount += this._field[doubleI - 1, j];
+								break;
+							}
+							else if (this._field[doubleI - 1, j] == 0)
+							{
+								uint val = this._field[doubleI, j];
+								this._field[doubleI, j] = this._field[doubleI - 1, j];
+								this._field[doubleI - 1, j] = val;
+								doubleI--;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		public void PushDown()
+		{
+			for (int i = 2; i >= 0; i--)
+			{
+				for (int j = 0; j < COUNT_ROWS; j++)
+				{
+					if (this._field[i, j] != 0)
+					{
+						int doubleI = i;
+
+						while (doubleI < COUNT_COLLUMNS - 1)
+						{
+							if (this._field[doubleI, j] != this._field[doubleI + 1, j] && this._field[doubleI + 1, j] != 0)
+							{
+								break;
+							}
+							if (this._field[doubleI, j] == this._field[doubleI + 1, j])
+							{
+								this._field[doubleI + 1, j] *= 2;
+								this._field[doubleI, j] = 0;
+								this._gameСount += this._field[doubleI + 1, j];
+								break;
+							}
+							else if (this._field[doubleI + 1, j] == 0)
+							{
+								uint val = this._field[doubleI, j];
+								this._field[doubleI, j] = this._field[doubleI + 1, j];
+								this._field[doubleI + 1, j] = val;
+								doubleI++;
+							}
+						}
+					}
+				}
+
+
+			}
+
+		}
+
+		public void PushRight()
+		{
+
+			for (int i = 2; i >= 0; i--)
+			{
+				for (int j = 0; j < COUNT_ROWS; j++)
+				{
+					if (this._field[j, i] != 0)
+					{
+						int doubleI = i;
+
+						while (doubleI < COUNT_COLLUMNS - 1)
+						{
+							if (this._field[j, doubleI] != this._field[j, doubleI + 1] && this._field[j, doubleI + 1] != 0)
+							{
+								break;
+							}
+
+							if (this._field[j, doubleI] == this._field[j, doubleI + 1])
+							{
+								this._field[j, doubleI + 1] *= 2;
+								this._field[j, doubleI] = 0;
+								this._gameСount += this._field[j, doubleI + 1];
+								break;
+							}
+							else if (this._field[j, doubleI + 1] == 0)
+							{
+								uint val = this._field[j, doubleI];
+								this._field[j, doubleI] = this._field[j, doubleI + 1];
+								this._field[j, doubleI + 1] = val;
+								doubleI++;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		public void PushLeft()
+		{
+			for (int i = 1; i < COUNT_ROWS; i++)
+			{
+				for (int j = 0; j < COUNT_COLLUMNS; j++)
+				{
+					if (this._field[j, i] != 0)
+					{
+						int doubleI = i;
+
+						while (doubleI > 0)
+						{
+							if (this._field[j, doubleI] != this._field[j, doubleI - 1] && this._field[j, doubleI - 1] != 0)
+							{
+								break;
+							}
+
+							if (this._field[j, doubleI] == this._field[j, doubleI - 1])
+							{
+								this._field[j, doubleI - 1] *= 2;
+								this._field[j, doubleI] = 0;
+								this._gameСount += this._field[j, doubleI - 1];
+								break;
+							}
+							else if (this._field[j, doubleI - 1] == 0)
+							{
+								uint val = this._field[j, doubleI];
+								this._field[j, doubleI] = this._field[j, doubleI - 1];
+								this._field[j, doubleI - 1] = val;
+								doubleI--;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		public uint[,] GetField()
+		{
+			return this._field;
+
+		}
+
+	}
 }
