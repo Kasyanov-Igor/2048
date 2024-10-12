911bc5138dcc049595bcf98c3a3ed0070910eac5
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2024-10-12 14:38:45 +0300

    Added GraphicalTile class. Closed #9

---
+++ 2048/Graphics/GraphicalTile .cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace _2048
+{
+	public class GraphicalTile : TextBlock
+	{
+		private const byte CELL_SIZE = 50;
+		public GraphicalTile(uint n, Grid a)
+		{
+			this.FontSize = CELL_SIZE;
+			this.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+			this.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+
+			//this.Width = 50;
+			//this.Width = a.Width / LogicGameField.COUNT_COLLUMNS; // ???
+
+			this.Text = Convert.ToString(n);
+			GetColor(n);
+
+		}
+
+		public void GetColor(uint number)
+		{
+			switch (number)
+			{
+				case 2:
+
+					this.Background = new SolidColorBrush(Color.FromArgb(24, 153, 0, 0));
+
+					break;
+
+				case 4:
+
+					this.Background = new SolidColorBrush(Color.FromArgb(72, 153, 0, 0));
+
+					break;
+				case 8:
+
+					this.Background = new SolidColorBrush(Color.FromArgb(96, 153, 0, 0));
+
+					break;
+
+				case 16:
+
+					this.Background = new SolidColorBrush(Color.FromArgb(120, 153, 0, 0));
+
+					break;
+				case 32:
+
+					this.Background = new SolidColorBrush(Color.FromArgb(144, 153, 0, 0));
+
+					break;
+
+				case 64:
+
+					this.Background = new SolidColorBrush(Color.FromArgb(168, 153, 0, 0));
+
+					break;
+				case 128:
+
+					this.Background = new SolidColorBrush(Color.FromArgb(192, 153, 0, 0));
+
+					break;
+
+				case 256:
+
+					this.Background = new SolidColorBrush(Color.FromArgb(216, 153, 0, 0));
+
+					break;
+				case 512:
+
+					this.Background = new SolidColorBrush(Color.FromArgb(230, 204, 0, 0));
+
+					break;
+
+				case 1024:
+
+					this.Background = new SolidColorBrush(Color.FromArgb(240, 204, 0, 0));
+
+					break;
+
+				case 2048:
+
+					this.Background = new SolidColorBrush(Color.FromArgb(255, 204, 0, 0));
+
+					break;
+			}
+		}
+	}
+}
+
+
---
+++ 2048/Graphics/GridGameField.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace _2048
+{
+	public class GridGameField : GridGame
+	{
+		public GridGameField(Window parentElement) : base()
+		{
+			for (byte i = 0; i < LogicGameField.COUNT_ROWS; i++)
+			{
+				this.RowDefinitions.Add(new RowDefinition());
+				this.ColumnDefinitions.Add(new ColumnDefinition());
+			}
+
+			this._gameField.AddField();
+			this._gameField.AddField();
+
+			this.Height = parentElement.Height * 0.80;
+			this.Margin = new Thickness(20);
+
+			this.ShowGridLines = true;
+
+			this.Render();
+		}
+
+		public override void Render()
+		{
+			uint[,] logicalGameField = this._gameField.GetField();
+
+			for (int i = 0; i < LogicGameField.COUNT_ROWS; i++)
+			{
+				for (int k = 0; k < LogicGameField.COUNT_COLLUMNS; ++k)
+				{
+					if (logicalGameField[i, k] != 0)
+					{
+						GraphicalTile textBox = new GraphicalTile(logicalGameField[i, k], this);
+                    
+						Grid.SetRow(textBox, i);
+						Grid.SetColumn(textBox, k);
+						this.Children.Add(textBox);
+					}
+				}
+			}
+		}
+
+
+		public override void Drawing()
+		{
+			this.Children.Clear();
+
+			if (!this._gameField.IsEmptyCell())
+			{
+				MessageBox.Show("Game over");
+			}
+			this.Render();
+		}
+	}
+}
